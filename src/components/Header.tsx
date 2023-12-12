ec935a13ba4ef395b725a95e594f61e573a7c8d2
ehababdelmawla <eabdo1474@gmail.com>
2023-12-12 09:05:54 +0200

    update text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export const Header=({isHideText,setHideText}:{isHideText:boolean,setHideText:Fu
     <header className={`${isHideText?'hidden':''}`}>
         
         <p>A foggy year can hold the most appreciable blessings as well!</p>
-        <p>Drag All the clouds right and left to reveal them   <button className='ok-button' onClick={()=>{
+        <p>Drag all the clouds right and left to reveal them   <button className='ok-button' onClick={()=>{
             setHideText(true)
         }}>ok</button></p>
       
